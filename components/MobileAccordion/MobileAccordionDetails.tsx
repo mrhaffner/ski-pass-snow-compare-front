e46a61fcc0272fa68d534049a7a3f43b3ea4b5dc
mrhaffner <mrhaffner13@gmail.com>
2021-10-25 18:31:49 -0500

    style: adjusted wind column layout

--- components/MobileAccordion/MobileAccordionDetails.tsx
+++ components/MobileAccordion/MobileAccordionDetails.tsx
@@ -15,17 +15,38 @@ const MobileAccordionDetails = ({ weather, day }: Props) => {
         <Typography sx={{ flexShrink: 0, alignSelf: 'center', width: '25%' }}>
           {day}
         </Typography>
-        <Box sx={{ display: 'flex', width: '25%' }}>
+        <Box sx={{ display: 'flex', width: '20%' }}>
           <AcUnit sx={{ fontSize: 18, alignSelf: 'center' }} />
           <Typography sx={{ flexShrink: 0, alignSelf: 'center' }}>
             {weather.snow}"
           </Typography>
         </Box>
-        <Box sx={{ display: 'flex', width: '25%' }}>
-          <Air sx={{ fontSize: 20, alignSelf: 'center' }} />
-          <Typography sx={{ flexShrink: 0, alignSelf: 'center' }}>
-            {weather.wind_gust_spd} mph
-          </Typography>
+        <Box
+          sx={{
+            width: '30%',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              maxWidth: '75px',
+            }}
+          >
+            <Air sx={{ fontSize: 20, alignSelf: 'center' }} />
+            <Box sx={{ display: 'flex', gap: '2px' }}>
+              <Typography sx={{ flexShrink: 0, alignSelf: 'center' }}>
+                {weather.wind_gust_spd}
+              </Typography>
+              <Typography
+                variant="body2"
+                component="span"
+                sx={{ flexShrink: 0, alignSelf: 'center' }}
+              >
+                mph
+              </Typography>
+            </Box>
+          </Box>
         </Box>
         <Box
           sx={{
